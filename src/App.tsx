05ea4ec71058c7decf13f0d142ef20ace200ae17
mateowall16 <carlosmateobruno@sempreceub.com>
2025-12-25 17:19:52 -0300

    3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+console.log("SUPABASE URL", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE KEY", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
